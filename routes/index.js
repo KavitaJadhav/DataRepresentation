7ae7a2a70358b98109b57dc539e3e41e78c98080
Shital Fattesing Mane <shitalmane639@gmail.com>
2014-11-03 17:39:15 +0530

    Prajakta/Shital: #1004 Connectivity of home page done with rendering on same page for taking user input.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,3 @@
 exports.home = function(req, res){
   res.render('home');
 };
-
-exports.bar_chart = function(req, res) {
-	res.render('bar_chart_input');
-}
-
-exports.pie_chart = function(req, res) {
-	res.render('pie_chart_input');
-}
